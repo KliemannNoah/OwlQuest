eec3695dc63d63c3cfd1b6c30ea816505ba70954
NoahKliemann <Packerfan504@gmail.com>
2019-04-22 13:17:04 -0500

    Updated the game with the new art assets

--- Owl Quest/Assets/Scripts/StartMenu.cs
+++ Owl Quest/Assets/Scripts/StartMenu.cs
@@ -17,7 +17,31 @@ public class StartMenu : MonoBehaviour
 		} if(isStart) {
 			StaticStart.numberOfPlayers = playerNumber;
 			SceneManager.LoadScene("MainScene");		
-			GetComponent<Renderer>().material.color=Color.cyan;
+			GetComponent<Renderer>().material.color=Color.green;
 		}
 	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit2D hit = Physics2D.GetRayIntersection (ray, Mathf.Infinity);
+			if (hit.collider != null && hit.collider.name == name) {
+                if (hit.collider.gameObject.tag == "Owl1"){
+                    playerNumber = 1;
+					PlayGame();
+				}else if (hit.collider.gameObject.tag == "Owl2"){
+                    playerNumber = 2;
+					PlayGame();					
+				}else if (hit.collider.gameObject.tag == "Owl3"){
+                    playerNumber = 3;
+					PlayGame();					
+				}else if (hit.collider.gameObject.tag == "Owl4"){
+                    playerNumber = 4;
+					PlayGame();					
+				}
+			}
+		}
+    }
 }
