0d87eee4ebc9f816acc36f3069cde645cdf89d79
Cindy <prentlikes2cookies@gmail.com>
2019-04-23 09:27:12 -0500

    Location Transparency

---
+++ Owl Quest/Assets/Scripts/LocationAnimations.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocationAnimations : MonoBehaviour
+{
+    Animator animator;
+    Renderer renderer;
+    public int location;
+    public backend b;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        renderer = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(b.occupied[location] == 0)
+        {
+            animator.SetInteger("AnimState", 0);
+            renderer.material.color = new Color(1.0f,1.0f,1.0f,1.0f);
+        }
+        else
+        {
+            animator.SetInteger("AnimState", 1);
+            renderer.material.color = new Color(1.0f, 1.0f, 1.0f, 0.5f);
+        }
+    }
+}
