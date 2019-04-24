8314a38142f5cbee8a8291fee39f09bb2a765d1c
NoahKliemann <Packerfan504@gmail.com>
2019-04-24 08:42:24 -0500

    Resume.cs

---
+++ Owl Quest/Assets/Scripts/Resume.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Resume : MonoBehaviour
+{
+	
+	public bool isRestart;
+	public bool isQuit;
+	public bool isMenu;
+	public Text Winner;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		
+    }
+	
+	
+	public void PlayGame() {
+		if(isQuit) {
+			Application.Quit();
+		}
+		if(isRestart) {
+			SceneManager.LoadScene("MainScene");		
+		}		
+		if(isMenu) {
+			SceneManager.LoadScene("StartScreen");			
+		}
+		
+		
+	}
+}
